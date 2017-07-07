2474e60602b29c31175705a4d95df8cdd440bb29
Diego Dias <diegodias@diegodias.local>
2017-07-07 02:11:27 -0300

    final commit

--- src/app/allrepos.js
+++ src/app/allrepos.js
@@ -105,7 +105,9 @@ export class Allrepos extends Component {
   handleLoadMore() {
     // HANDLER DO BOTAO CARREGAR MAIS SE FOR MAIOR = QUE O NUMERO DE REPOS HIDE CARREGAR MAIS 20 NO CLICK
     this.commitCounter();
-    console.log(this.state.limit + 5);
+    if ((this.state.limit + 5) >= (this.state.commitsUrl.length)) {
+      this.setState({showLoad: !this.state.showLoad});
+    }
   }
   commitCounter() {
     if ((this.state.limit) >= (this.state.commitsUrl.length)) {
